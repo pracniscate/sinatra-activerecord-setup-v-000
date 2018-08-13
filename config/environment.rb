b80c2bbbece0ed98fa0fbefd93cea679b62612e2
Lorena <lorena.giddens@outlook.com>
2018-08-13 03:33:11 +0000

    automatically backed up by learn

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,7 @@ require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
 # sets up a connection to a sqlite3 db named "database.db"
-configure :development do 
+configure :development do
   set :database, 'sqlite3:db/database.db'
 end
 
