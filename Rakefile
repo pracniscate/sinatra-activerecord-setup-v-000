51b6b8ac20642509369b51545ebba1d5e9ff431b
Lorena <lorena.giddens@outlook.com>
2018-08-13 03:39:20 +0000

    Done.

---
+++ Rakefile
@@ -0,0 +1,2 @@
+require './config/environment' # loads environment
+require 'sinatra/activerecord/rake' # gets Rake tasks from the sinatra-activerecord gem
--- config/environment.rb
+++ config/environment.rb
@@ -3,4 +3,9 @@ ENV['SINATRA_ENV'] ||= "development"
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
+# sets up a connection to a sqlite3 db named "database.db"
+configure :development do
+  set :database, 'sqlite3:db/database.db'
+end
+
 require './app'
